d8bbd31f7922b34505678ed461ca0e584ec7cabf
OguzhanCetinn <16050111050@ybu.edu.tr>
2020-06-19 15:31:47 +0300

    Commited

---
+++ StaticWebPage/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StaticWebPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("About")]
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [Route("Products")]
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+        [Route("Store")]
+        public ActionResult Store()
+        {
+            return View();
+        }
+
+    }
+}
